88b1e1f1860245ddb79b5781c058e1d75a76e481
Greg Haygood <ghaygood@gmail.com>
2019-07-30 16:27:41 -0400

    updated to use axios & circus starter

---
+++ dist/js/main.js
@@ -0,0 +1,4 @@
+console.log(`Hello World from main.js! 
+Change this message, and make sure it changes in the browser 
+to verify that you're working in the right files.`);
+//# sourceMappingURL=main.js.map
